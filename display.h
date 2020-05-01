3f90eb45c2d994d0db5a81a9a36d0fc79a8db354
codybutterfield82 <52293290+codybutterfield82@users.noreply.github.com>
2020-04-30 20:03:50 -0600

    Create display.h

---
+++ display.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+class display{
+  public:
+    void displayLetters(string);
+    void displayGuessedLetters(string);
+    void displayLives(int);
+    display();
+  private:
+
+};
